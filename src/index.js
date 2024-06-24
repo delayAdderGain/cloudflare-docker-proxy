994b81ce3295d635956f1569e9027783c6c7926f
delayAdderGain <57170829+delayAdderGain@users.noreply.github.com>
2024-06-24 15:31:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.floppin.xyz": dockerHub,
+  "quay.floppin.xyz": "https://quay.io",
+  "gcr.floppin.xyz": "https://gcr.io",
+  "k8s-gcr.floppin.xyz": "https://k8s.gcr.io",
+  "k8s.floppin.xyz": "https://registry.k8s.io",
+  "ghcr.floppin.xyz": "https://ghcr.io",
+  "cloudsmith.floppin.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.floppin.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
